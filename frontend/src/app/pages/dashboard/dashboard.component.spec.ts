2ccbc67d5ccae91fc900cdfc9685b012e0c4ae39
Cesar <113320974+Elemento935@users.noreply.github.com>
2025-11-24 11:35:48 -0600

    Create dashboard.component.spec.ts

---
+++ frontend/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DashboardComponent] // ✅ Como es standalone, se importa directamente
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
